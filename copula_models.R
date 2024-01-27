7a302f2eec93d37693a9bc634e3dacdb88aca7c0
William Burgess <williamburgess02@gmail.com>
2024-01-27 18:42:28 +0000

    Update copula_models.R

--- copula_models.R
+++ copula_models.R
@@ -9,3 +9,9 @@ cop_res <- cop_res / max(cop_res)
 cop_fit1 <- RVineStructureSelect(as.copuladata(cop_res))
 
 summary(cop_fit1)
+
+
+copres <- cbind(y_gar_res, y2_gar_res, y3_gar_res, y4_gar_res, y5_gar_res, y6_gar_res)
+copres <- copres - min(copres)
+copres <- copres / max(copres)
+cop_fit2 <- RVineStructureSelect(as.copuladata(copres))
